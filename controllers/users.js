ad02cbca0f5f438d794caeef48f740e5886eb54c
sergey <sapokrovsky@gmail.com>
2023-03-18 03:47:40 +0300

    feat: samsite

--- controllers/users.js
+++ controllers/users.js
@@ -97,6 +97,7 @@ const login = async (req, res, next) => {
     return res.cookie('token', token, {
       maxAge: 3600000 * 24 * 7,
       httpOnly: true,
+      sameSite: 'None',
     })
       .send({ messge: 'Успешная авторизация' });
   } catch (err) {
